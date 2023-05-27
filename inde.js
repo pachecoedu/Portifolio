5a16e5b6ba33c3126f2d813cc84468f21178f069
Carlos Eduardo <92826753+pachecoedu@users.noreply.github.com>
2023-05-27 16:57:40 -0300

    limitando a quantidade de divs criadas

--- inde.js
+++ inde.js
@@ -1,11 +1,17 @@
 // Chame o elemento pai onde as divs serão adicionadas
 var elementoPai = document.getElementById("elemento-pai");
 
-// Número de divs que você deseja criar
-var numeroDivs = 50;
+// Número máximo de divs que você deseja criar
+var numeroMaxDivs = 200;
+var divsCriadas = 0; // Contador de divs criadas
 
 // Função para criar e animar uma div
 function criarDiv() {
+  // Verificar se o número máximo de divs foi atingido
+  if (divsCriadas >= numeroMaxDivs) {
+    return;
+  }
+
   // Criando a div
   var div = document.createElement("div");
 
@@ -18,8 +24,6 @@ function criarDiv() {
   div.style.width = "2px";
   div.style.borderRadius = "50%";
   div.style.height = Math.random() * 10 + "px";
-
-  // Definindo a
   div.style.backgroundColor = "#9624f9b3";
 
   // Adicionando a classe "chuva" à div
@@ -27,11 +31,17 @@ function criarDiv() {
 
   // Adicionando o evento de finalização de animação
   div.addEventListener("animationend", function () {
-    div.remove();
+    if (div.getBoundingClientRect().bottom >= window.innerHeight) {
+      div.remove();
+      divsCriadas--;
+    }
   });
 
   // Adicionando a div ao elemento pai
   elementoPai.appendChild(div);
+
+  // Atualizar o contador de divs criadas
+  divsCriadas++;
 }
 
 function criarDivsIntervalo() {
